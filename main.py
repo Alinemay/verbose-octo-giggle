ce7f8a28dec0605a07c1fd88dc12186f190c70ad
Alinemay <112935940+Alinemay@users.noreply.github.com>
2023-07-11 16:36:59 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ def get_access_token():
 
 
 def get_weather(region):
+    # 天气
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
                       'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
